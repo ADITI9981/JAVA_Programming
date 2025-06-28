3e53ef842df60b1f9217b6e64a2b060e31eb325f
Aditi Yadav <munmunyadav9981@gmail.com>
2025-06-28 22:02:33 +0530

    Update 014_Array.java

--- 014_Array.java
+++ 014_Array.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+public class Array {
+    public static void main(String[] args) {
+        int [] marks = new int[5];
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<5;i++){
+            System.out.printf("Enter the marks %d :",i);
+            marks[i]=sc.nextInt();
+        }
+        System.out.println("The Array is:");
+        for(int i=0;i<5;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
 
