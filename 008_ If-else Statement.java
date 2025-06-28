643de7ff5980d836e84b4d27d93ba559a6e4f2d1
Aditi Yadav <munmunyadav9981@gmail.com>
2025-06-28 17:51:04 +0530

    Update 008_ If-else Statement.java

--- 008_ If-else Statement.java
+++ 008_ If-else Statement.java
@@ -1,1 +1,12 @@
+public class Conditionals {
+    public static void main(String[] args) {
+          int age = 6;
+          if(age>18){
+              System.out.println("yes ypu can drive");
+          }
+          else{
+              System.out.println("no you can't drive");
+          }
+    }
+}
 
