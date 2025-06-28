78bcf519a01f22dd083c214817c9f41f1b4bedeb
Aditi Yadav <munmunyadav9981@gmail.com>
2025-06-28 21:26:38 +0530

    Update 011_Do_While_loop.java

--- 011_Do_While_loop.java
+++ 011_Do_While_loop.java
@@ -1,1 +1,10 @@
+public class Do_While_Loop {
+    public static void main(String[] args) {
+        int a=10;
+        do{
+            System.out.println(a);
+            a++;
+        }while(a<5);
+    }
+}
 
