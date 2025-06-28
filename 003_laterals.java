6438fa04f8f4663c6a3620f34cad3be09337d951
Aditi Yadav <munmunyadav9981@gmail.com>
2025-06-28 14:11:35 +0530

    Create 003_laterals.java

---
+++ 003_laterals.java
@@ -0,0 +1,10 @@
+public class CWH_04_laterals {
+    public static void main(String[] args){
+        int age = 34;
+        char ch='A';
+        short age3=67;
+        long ageDino = 3888888888L;
+        float f1= 5.4f;
+        System.out.println(age);
+    }
+}
