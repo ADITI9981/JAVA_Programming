b5781287398ac538e6914931a3f7f8ec26caa539
Aditi Yadav <munmunyadav9981@gmail.com>
2025-06-28 17:01:07 +0530

    Create 006_Strings.java

---
+++ 006_Strings.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class Strings {
+    public static void main(String[] args){
+        String name = new String("Aditi");
+        String lastname = "Yadav";
+        System.out.println(name+lastname);
+        int a=2;
+        int b=89;
+        System.out.printf("the value of a is %d and value of b is %d:",a,b);
+        System.out.format("the value of a is %d and value of b is %d:",a,b);
+
+    }
+}
