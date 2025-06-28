0418bb392d7121fde56de8aa39d6098329a16b54
Aditi Yadav <munmunyadav9981@gmail.com>
2025-06-28 13:10:07 +0530

    Create 002_Addition.java

---
+++ 002_Addition.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("sum of this number is: ");
+        int num1=1;
+        int num2=4;
+        int num3=8;
+        int sum =num1+num2+num3;
+        System.out.println(sum);
+    }
+}
