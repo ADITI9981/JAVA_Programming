71f11df10216f4828d1cb7dda51684f17a803c63
Aditi Yadav <munmunyadav9981@gmail.com>
2025-06-28 21:47:22 +0530

    Update 013_Break_And_Continue.java

--- 013_Break_And_Continue.java
+++ 013_Break_And_Continue.java
@@ -1,1 +1,14 @@
+public class break_And_Continue {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            System.out.println(i);
+            System.out.println("java is great");
+            
+            if(i==2){
+                System.out.println("Ending the loop");
+                break;
+            }
+        }
+    }
+}
 
