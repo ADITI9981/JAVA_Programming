8181bff4ab7bc38d67fa268b0d95ad89c1c4958a
Aditi Yadav <munmunyadav9981@gmail.com>
2025-06-29 14:42:46 +0530

    Update 018_varargs.java

--- 018_varargs.java
+++ 018_varargs.java
@@ -1,1 +1,17 @@
+public class varargs {
+
+    static int sum(int ...arr){
+        int result=0;
+        for(int a:arr){
+            result+=a;
+        }
+        return result;  
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Varargs");
+        System.out.println("the sum of 2,3 is:"+sum(2,3));
+        System.out.println("the sum of 2,3 and 5 is:"+sum(2,3,5));
+        System.out.println("the sum of 2,3,5 and 9 is:"+sum(2,3,5,9));
+    }
+}
 
