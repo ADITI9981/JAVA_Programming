a38cb96f553b2a5095a4cf2481c41a09b3d0dc9b
Aditi Yadav <munmunyadav9981@gmail.com>
2025-06-29 14:03:34 +0530

    Update 016_methods.java

--- 016_methods.java
+++ 016_methods.java
@@ -1,1 +1,25 @@
+public class methods {
+    static int logic(int x, int y){
+        int z;
+        if(x>y){
+            z= x+y;
+        }
+        else{
+            z= (x+y) * 5;
+        }
+        return z;
+    }
+    public static void main(String[] args) {
+        int a=5;
+        int b=7;
+        int c = logic(a,b);
+        int a1=9;
+        int b1=6;
+        int c1 = logic(a1,b1);
+        System.out.printf("c: %d",c);
+        System.out.println("");
+        System.out.printf("c1: %d",c1);
+    }
+
+}
 
